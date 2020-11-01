04dc9911201447061cfe4933e46300e9f7b6c50c
Weidong Lian <weidong.lian@gmail.com>
2020-11-01 22:53:47 +0100

    test: add graphql notes and todo tests

---
+++ graph/notes_test.go
@@ -0,0 +1,168 @@
+package graph_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/rs/xid"
+	"github.com/weidonglian/golang-notes-app/model"
+	"github.com/weidonglian/golang-notes-app/store"
+	"github.com/weidonglian/golang-notes-app/test"
+	"net/http"
+)
+
+var _ = Describe("Graph Notes", func() {
+
+	var (
+		testApp       test.TestApp
+		notesStore    store.NotesStore
+		testUserId    int
+		devUserId     int
+		testUserNotes []model.NoteWithTodos
+		devUserNotes  []model.NoteWithTodos
+	)
+
+	BeforeEach(func() {
+		testApp = test.NewTestAppAndServe()
+		notesStore = testApp.App.GetStore().Notes
+		testUserId = testApp.App.GetStore().Users.FindByName("test").ID
+		devUserId = testApp.App.GetStore().Users.FindByName("dev").ID
+		testUserNotes = test.NewTestUserNotesData(&testApp)
+		devUserNotes = test.NewDevUserNotesData(&testApp)
+	})
+
+	AfterEach(func() {
+		testApp.Close()
+	})
+
+	It("query notes", func() {
+		Context("should fetch notes of test user, should not include notes of dev user", func() {
+			fetchedNotes := testApp.GraphMustData(`query{
+			  notes {
+				id
+				name    
+				todos {
+				  id
+				  name
+				  done
+				  noteId
+				}
+			  }
+            }`).ContainsKey("notes").Value("notes").Array()
+
+			fetchedNotes.Length().Equal(len(testUserNotes))
+			for i := range testUserNotes {
+				fetchedNote := fetchedNotes.Element(i).Object()
+				fetchedNote.Keys().Contains("id", "name", "todos").NotContains("userId")
+				fetchedNote.Values().Contains(testUserNotes[i].ID, testUserNotes[i].Name)
+				for _, devNote := range devUserNotes {
+					fetchedNote.Values().NotContains(devNote.ID, devNote.Name)
+				}
+				fetchedTodos := fetchedNote.Value("todos").Array()
+				fetchedTodos.Length().Equal(len(testUserNotes[i].Todos))
+				for j := range testUserNotes[i].Todos {
+					fetchedTodos.Element(j).Object().Keys().Contains("id", "name", "done", "noteId")
+					fetchedTodos.Element(j).Object().Equal(testUserNotes[i].Todos[j])
+				}
+			}
+		})
+	})
+
+	It("query note by id", func() {
+		Context("we should be able to get the test users notes by id", func() {
+			for i := range testUserNotes {
+				fetchedNote := testApp.GraphMustData(`query($id: Int!){
+				  note(id: $id) {
+					id
+					name    
+					todos {
+					  id
+					  name
+					  done
+					  noteId
+					}
+				  }
+				}`, map[string]interface{}{
+					"id": testUserNotes[i].ID,
+				}).ContainsKey("note").Value("note").Object()
+				fetchedNote.Value("id").Equal(testUserNotes[i].ID)
+				fetchedNote.Value("name").Equal(testUserNotes[i].Name)
+				fetchedNote.Value("todos").Equal(testUserNotes[i].Todos)
+				fetchedNote.NotContainsKey("userId")
+			}
+		})
+
+		Context("we are not allowed to fetch another user's resources", func() {
+			// testApp.API is authenticated for 'test' user should not get notes of 'dev' user even the note id is valid
+			for i := range devUserNotes {
+				testApp.API.GET("/notes/{id}", devUserNotes[i].ID).
+					Expect().
+					Status(http.StatusUnprocessableEntity).Body().Contains("unable to find note")
+			}
+		})
+	})
+
+	It("POST /notes", func() {
+		Context("we should be able to create name with any non-empty string", func() {
+			noteNames := []string{"pn1", "pn2", "pn3"}
+			for _, noteName := range noteNames {
+				newNote := testApp.API.POST("/notes").WithJSON(map[string]string{"name": noteName}).
+					Expect().
+					Status(http.StatusOK).JSON().Object()
+				newNote.Keys().Contains("id", "name", "todos").NotContains("userId")
+				newNote.Value("name").Equal(noteName)
+				newNote.Value("todos").Array().Length().Equal(0)
+			}
+		})
+
+		Context("empty note name is not allowed to create", func() {
+			testApp.API.POST("/notes").WithJSON(map[string]string{"name": ""}).
+				Expect().
+				Status(http.StatusBadRequest).JSON().Object().Value("error").String().Contains("missing required fields")
+			testApp.API.POST("/notes").
+				Expect().
+				Status(http.StatusBadRequest).Body().Contains("unable to automatically decode the request content type")
+		})
+	})
+
+	It("PUT /notes/{id}", func() {
+		Context("we should be able to update note name", func() {
+			for _, note := range testUserNotes {
+				randomName := xid.New().String()
+				updatedNote := testApp.API.PUT("/notes/{id}", note.ID).WithJSON(map[string]string{"name": randomName}).
+					Expect().
+					Status(http.StatusOK).JSON().Object()
+				updatedNote.Keys().Contains("id", "name", "todos").NotContains("userId")
+				updatedNote.Value("name").Equal(randomName)
+				updatedNote.Value("todos").Equal(note.Todos)
+			}
+		})
+	})
+
+	It("DELETE /notes/{id}", func() {
+		Context("we should be able to delete the test user's notes by id", func() {
+			for _, note := range testUserNotes {
+				testApp.API.DELETE("/notes/{id}", note.ID).
+					Expect().
+					Status(http.StatusOK).Body().Empty()
+			}
+		})
+
+		Context("we should not be able to delete valid id of another user", func() {
+			for _, note := range devUserNotes {
+				testApp.API.DELETE("/notes/{id}", note.ID).
+					Expect().
+					Status(http.StatusUnprocessableEntity).Body().Contains("unable to find note")
+			}
+		})
+	})
+
+	It("DELETE /notes", func() {
+		Context("we should be able to delete the test user's notes", func() {
+			testApp.API.DELETE("/notes").
+				Expect().
+				Status(http.StatusOK).Body().Empty()
+			Expect(len(notesStore.FindByUserID(testUserId))).To(BeZero())
+			Expect(len(notesStore.FindByUserID(devUserId))).NotTo(BeZero())
+		})
+	})
+})
---
+++ graph/todos_test.go
@@ -0,0 +1,157 @@
+package graph_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	"github.com/rs/xid"
+	"github.com/weidonglian/golang-notes-app/model"
+	"github.com/weidonglian/golang-notes-app/test"
+	"net/http"
+)
+
+var _ = Describe("Todos", func() {
+
+	var (
+		testApp       test.TestApp
+		testUserNotes []model.NoteWithTodos
+		devUserNotes  []model.NoteWithTodos
+	)
+
+	BeforeEach(func() {
+		testApp = test.NewTestAppAndServe()
+		testUserNotes = test.NewTestUserNotesData(&testApp)
+		devUserNotes = test.NewDevUserNotesData(&testApp)
+	})
+
+	AfterEach(func() {
+		testApp.Close()
+	})
+
+	It("GET /todos/{id}", func() {
+		Context("we should be able to get the test users todo by id", func() {
+			for i := range testUserNotes {
+				for j := range testUserNotes[i].Todos {
+					testApp.API.GET("/todos/{id}", testUserNotes[i].Todos[j].ID).
+						Expect().
+						Status(http.StatusOK).JSON().Object().Equal(testUserNotes[i].Todos[j])
+				}
+			}
+		})
+
+		Context("Non-existent ids should not work", func() {
+			for _, todoID := range []int{10000, 20000, 50, 19000000} {
+				testApp.API.GET("/todos/{id}", todoID).
+					Expect().
+					Status(http.StatusUnprocessableEntity).Body().Contains("unable to find todo")
+			}
+		})
+
+		Context("Bad {id} format should not crash the app", func() {
+			for _, todoID := range []interface{}{"not_a_id", "@@@@@", `////`, `___????^%`} {
+				testApp.API.GET("/todos/{id}", todoID).
+					Expect().
+					Status(http.StatusBadRequest)
+			}
+		})
+
+		Context("we are not allowed to fetch another user's resources", func() {
+			// testApp.API is authenticated for 'test' user should not get notes of 'dev' user even the note id is valid
+			for i := range devUserNotes {
+				for j := range devUserNotes[i].Todos {
+					testApp.API.GET("/todos/{id}", devUserNotes[i].Todos[j].ID).
+						Expect().
+						Status(http.StatusUnprocessableEntity).Body().Contains("unable to find todo for current user")
+				}
+			}
+		})
+	})
+
+	It("POST /todos", func() {
+
+		todoNames := []string{"pn_todo_1", "pn_todo_2", "pn_todo_3", "pn_todo_1"}
+
+		Context("we should be able to create with any non-empty string", func() {
+			for i := range testUserNotes {
+				for _, todoName := range todoNames {
+					fetchedObject := testApp.API.POST("/todos").WithJSON(map[string]interface{}{
+						"name":   todoName,
+						"done":   true,
+						"noteId": testUserNotes[i].ID,
+					}).Expect().
+						Status(http.StatusOK).JSON().Object()
+					fetchedObject.Keys().Contains("id", "name", "done", "noteId")
+					fetchedObject.Values().Contains(todoName, testUserNotes[i].ID, true)
+				}
+			}
+		})
+
+		Context("Invalid noteId should not be able to create any todo", func() {
+			for _, noteID := range []int{100, 200, 500, 999999} {
+				for _, todoName := range todoNames {
+					testApp.API.POST("/todos").WithJSON(map[string]interface{}{
+						"name":   todoName,
+						"todo":   true,
+						"noteId": noteID,
+					}).Expect().
+						Status(http.StatusUnprocessableEntity).Body().Contains("provide note with id does not exist for current user")
+				}
+			}
+		})
+	})
+
+	It("PUT /todos/{id}", func() {
+		Context("we should be able to update todo", func() {
+			for i := range testUserNotes {
+				for j := range testUserNotes[i].Todos {
+					randomName := xid.New().String()
+					obj := testApp.API.PUT("/todos/{id}", testUserNotes[i].Todos[j].ID).WithJSON(map[string]interface{}{
+						"name": randomName,
+						"done": true,
+					}).Expect().
+						Status(http.StatusOK).JSON().Object()
+					obj.Keys().Contains("id", "name", "done", "noteId")
+					obj.Values().Contains(testUserNotes[i].Todos[j].ID, randomName, true, testUserNotes[i].ID)
+				}
+			}
+		})
+	})
+
+	It("PUT /todos/{id}/toggle", func() {
+		Context("we should be able to toggle done", func() {
+			for i := range testUserNotes {
+				for j := range testUserNotes[i].Todos {
+					testApp.API.PUT("/todos/{id}/toggle", testUserNotes[i].Todos[j].ID).Expect().
+						Status(http.StatusOK).JSON().Object().Value("done").NotEqual(testUserNotes[i].Todos[j].Done)
+				}
+			}
+		})
+
+		Context("we should not be able to toggle another user's resources even valid ids", func() {
+			for i := range devUserNotes {
+				for j := range devUserNotes[i].Todos {
+					testApp.API.PUT("/todos/{id}/toggle", devUserNotes[i].Todos[j].ID).Expect().
+						Status(http.StatusUnprocessableEntity)
+				}
+			}
+		})
+	})
+
+	It("DELETE /todos/{id}", func() {
+		Context("we should be able to delete the test user's todo by id", func() {
+			for i := range testUserNotes {
+				for j := range testUserNotes[i].Todos {
+					testApp.API.DELETE("/todos/{id}", testUserNotes[i].Todos[j].ID).Expect().
+						Status(http.StatusOK)
+				}
+			}
+		})
+
+		Context("we should not be able to delete valid id of another user", func() {
+			for i := range devUserNotes {
+				for j := range devUserNotes[i].Todos {
+					testApp.API.DELETE("/todos/{id}", devUserNotes[i].Todos[j].ID).Expect().
+						Status(http.StatusUnprocessableEntity)
+				}
+			}
+		})
+	})
+})
